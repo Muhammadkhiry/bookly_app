cd7f32925be32ef8fc02ac3c8714a46803c9e68e
Muhammad-Khairy <mohamedkhiry121t@gmail.com>
2026-09-24 11:16:58 +0300

    adding the font and assets to the pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bookly App',
       theme: ThemeData(
         // This is the theme of your application.
         //
