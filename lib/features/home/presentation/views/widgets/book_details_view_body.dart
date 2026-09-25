b8ccc8ef5d0dde43c417d023bb72ca11b5218e09
Muhammad-Khairy <mohamedkhiry121t@gmail.com>
2026-09-25 09:22:22 +0300

    create the book details view, book details view body , add go router package

---
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsViewBody extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
